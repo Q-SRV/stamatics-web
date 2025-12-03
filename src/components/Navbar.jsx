dd4bdb7238a43415d2355ab6e5c34e4f397993f8
M Mithilesh <mithilehm24@iitk.ac.in>
2025-12-03 17:44:50 +0530

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,55 +22,23 @@ function Navbar() {
         {/* RIGHT NAVIGATION */}
         <nav className="nav-links top-right-nav">
 
-
           {/* ABOUT US */}
-
-          {/* About Us scrolls within Home for now */}
-
           <a href="/#about" className="nav-link">
             About Us
           </a>
 
-          {/* COMPETITIONS DROPDOWN */}
-          <div className="dropdown">
-            <span className="dropdown-label">Competitions ▾</span>
+          {/* COMPETITIONS */}
+          <NavLink to="/competitions" className={navLinkClass}>
+            Competitions
+          </NavLink>
 
-            <div className="dropdown-menu">
-              {/* These still scroll within Home for now */}
-              <a href="/#integration" className="dropdown-item">
-                Integration Bee
-              </a>
-              <a href="/#mathematica" className="dropdown-item">
-                Mathematica
-              </a>
-
-              {/* Mathemania now has its own page */}
-              <NavLink to="/mathemania" className="dropdown-item">
-                Mathemania
-              </NavLink>
-
-              <a href="/#participants" className="dropdown-item">
-                Participants
-              </a>
-            </div>
-          </div>
-
-
-          {/* BLOGS — FIXED: Now points to /blogs */}
+          {/* BLOGS */}
           <NavLink to="/blogs" className={navLinkClass}>
             Blogs
           </NavLink>
 
-          {/* TEAM — Still points to Home for now */}
-
-          {/* BLOGS – can be routed later */}
-          <NavLink to="/" className={navLinkClass}>
-            Blogs
-          </NavLink>
-
-          {/* TEAM – can be routed later */}
-
-          <NavLink to="/" className={navLinkClass}>
+          {/* OUR TEAM */}
+          <NavLink to="/team" className={navLinkClass}>
             Our Team
           </NavLink>
 
